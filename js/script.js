715fb2b267db8960f835475ba8f037ab05b259b7
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-01-03 00:54:39 -0500

    adds solution to js file

--- js/script.js
+++ js/script.js
@@ -10,9 +10,30 @@ $(document).ready( function() {
 var apiCall = function() {
   console.log('clicked');
   // add code here to make the AJAX request!
+  var keyword = $("input[name='keyword']").val();
+  var url = "https://www.omdbapi.com/?s="+keyword
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json"
+  }).done ( function(response){
+    console.log(response.Search);
+    // call movie function below to append movie titles
+    movie(response);
+  }).fail ( function (){
+    console.log("fail");
+  }).always( function(){
+    console.log("Something happens");
+  })
 }
 
 
 var movie = function(response) {
   // add code here to append data to the body!
+  for (var i=0;i<response.Search.length;i++) {
+    // append movie titles to body
+    $('body').append("<div class = movie-title></div>")
+    $('.movie-title').append("<p>"+response.Search[i].Title+"</p>")
+    $('.movie-title').append("<img src = '"+response.Search[i].Poster+"'>")
+  }
 }
